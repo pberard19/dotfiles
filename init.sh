51406fb825fda72cbb6a9a3e5b9fc40e9846b226
Pat Berard <patrickjamesberard@gmail.com>
2022-04-04 14:15:34 -0500

    [UPDATE] Add pokemonsay & fortune setup

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ if ! command -v brew &> /dev/null
 then
 	echo "homebrew could not be found - attempting to install it. You may be prompted for your password"
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	
+
 	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/pb/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 
@@ -17,5 +17,38 @@ then
 else
 	echo "homebrew is already installed"
 fi
+
+echo "Checking fortune"
+if ! command -v fortune &> /dev/null
+then
+	echo "Installing fortune - this may take a little while"
+	brew install fortune
+
+	if ! command -v fortune &> /dev/null
+	then
+		echo "fortune could not be installed"
+		exit
+	fi
+else
+	echo "fortune is already installed"
+fi
+
+echo "Checking pokemonsay"
+if ! command -v pokemonsay &> /dev/null
+then
+	echo "Installing pokemonsay"
+	brew tap possatti/possatti
+	brew install pokemonsay
+
+	if ! command -v pokemonsay &> /dev/null
+	then
+		echo "pokemonsay could not be installed - see https://github.com/possatti/pokemonsay"
+		exit
+	fi
+else
+	echo "pokemonsay is already installed"
+fi
+
+
 
 echo "Setup Complete"
